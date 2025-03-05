3ede8805f826a0e84cfcff64b6b411d63abeb950
Viktor Senyk <viktorsenyk@Nadins-MacBook-Air.local>
2025-03-05 20:13:15 +0200

    Added Playwright HTML report deployment to GitHub Pages

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,10 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]], // Додаємо HTML-репорт
+  use: {
+    headless: true, // Запуск тестів у headless-режимі
+  },
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -22,15 +26,15 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+  // reporter: 'html',
+  // /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  // use: {
+  //   /* Base URL to use in actions like `await page.goto('/')`. */
+  //   // baseURL: 'http://127.0.0.1:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-  },
+  //   /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+  //   trace: 'on-first-retry',
+  // },
 
   /* Configure projects for major browsers */
   projects: [
